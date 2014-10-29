b09ebdb97c5b6f386ac4fb5bce3ff96782c5b14b
Jeffrey Hu <zhiwehu@gmail.com>
2014-10-30 00:12:57 +0800

    user.avatar_url

--- zhiwehu/users/models.py
+++ zhiwehu/users/models.py
@@ -11,6 +11,7 @@ from django.utils.translation import ugettext_lazy as _
 # Subclass AbstractUser
 class User(AbstractUser):
     website = models.URLField(max_length=255, blank=True, null=True)
+    avatar_url = models.URLField(max_length=255, blank=True, null=True)
 
     def __unicode__(self):
         return self.username
