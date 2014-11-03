0057aa8392a96f03306af1fc388b053c7b93b8c5
Jeffrey Hu <zhiwehu@gmail.com>
2014-11-04 00:50:59 +0800

    USE_TZ = False

--- zhiwehu/config/common.py
+++ zhiwehu/config/common.py
@@ -153,7 +153,7 @@ class Common(Configuration):
     USE_L10N = True
 
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#use-tz
-    USE_TZ = True
+    USE_TZ = False
     # END GENERAL CONFIGURATION
 
     # TEMPLATE CONFIGURATION
